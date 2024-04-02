40d68aa6f719a0c5d687162cfb0c9d17d0de0993
AtlasCount97 <raffaele.ancarola@protonmail.com>
2024-04-02 08:29:42 +0200

    Implementation (non-tested) of mass matrix

--- 2d/src/fe/components/mass.cpp
+++ 2d/src/fe/components/mass.cpp
@@ -8,42 +8,101 @@ std::array<std::function<double(const vertex_t&)>, 3> MassComponent::basis = {
         [](const vertex_t& z) -> double { return z[1]; }
       };
 
-cpx_matrix MassComponent::generate_matrix() const {
-  
-  // initialize with a null matrix 
-  cpx_matrix M = cpx_matrix::Zero(N_vertex, N_vertex);
+
+
+Mass::Mass(
+    std::weak_ptr<const AbstractMesh> mesh, 
+    const ScalarField& field,
+    std::shared_ptr<const Integrator> integrator
+    ) 
+  : MatrixComponent(), FEComponent(mesh, field), integrator(integrator)
+{
+}
+
+matrix Mass::generate_matrix() const {
+
+  auto mesh_ptr = mesh.lock();
+
+  const index_t N_active_vertex = mesh_ptr->n_active_vertices();
+  const index_t N_triangles = mesh_ptr->n_triangles();
+
+  // initialize with a null matrix
+  matrix M = matrix::Zero(N_active_vertex, N_active_vertex);
 
   for (index_t k = 0; k < N_triangles; ++k) {
-    
-    for(index_t i = 0; i < interp.size(); ++i) {
-      for(index_t j = 0; j < interp.size(); ++j) {
-      
-        // determine vertex indexes
-        const index_t N_i = contributions[k].vert_indexes[i];
-        const index_t N_j = contributions[k].vert_indexes[j];
 
-        // compute metrics
-        double metric = contributions[k].abs_detB;
+    // get triangle structure
+    const triangle_t& K = mesh.lock()->get_triangle(k);
 
-        // integrate by interpolation
-        qsim2d::complex integral = 0.;
-      
-        for (index_t l = 0; l < interp.size(); ++l) {
+    // get pre-computed metric structure
+    const auto& metric = metrics[k];
 
-          // evaluate local basis
-          double phi_il = basis[i](interp[l].location);
-          double phi_jl = basis[j](interp[l].location);
+    // store triangle contribution
+    Eigen::Matrix3d M_k;
 
-          // phi_i * phi_j * det(B_k)
-          integral += phi_il * phi_jl * contributions[k].field_evals[l];
-        }
+    for(index_t i = 0; i < 3; ++i) {
 
-        // update matrix
-        M(N_i, N_j) += metric * integral;
+      for(index_t j = 0; j < 3; ++j) {
+
+        // integrate field * basis_i over unit triangle
+        double field_integral = integrator->integrate([&](vertex_t z) -> double {
+          return field(metric.transform(z)) * basis[i](z) * basis[j](z);
+        }); 
+
+
+        // update local matrix
+        M_k(i,j) = metric.abs_detB * field_integral;
       }
     }
+
+    // update global matrix
+    for(index_t i = 0; i < 3; ++i) {
+
+      // determine target in the edge
+      const index_t N_i = K[i];
+
+      // if N_i is out of scope: do nothing
+      if (N_i >= N_active_vertex) {
+        continue;
+      }
+
+      for (index_t j = 0; j < 3; ++j) {
+
+        // determine target in the edge
+        const index_t N_j = K[j];
+
+        // if N_j is out of scope: do nothing
+        if (N_j >= N_active_vertex) {
+          //boundary_index.push_back(N_j);
+          //boundary = true;
+          continue;
+        } 
+        
+        // assign contribution
+        M(N_i, N_j) += M_k(i,j);
+      }
+    }
+
   } 
   
-  return M;
+  return l;
+}
+
+
+NeumannMass::NeumannMass(
+          std::weak_ptr<const Mesh> mesh, 
+          const ScalarField& field,
+          std::shared_ptr<const Integrator> integrator) 
+  : Mass(std::static_pointer_cast<const AbstractMesh>(mesh.lock()), field, integrator)
+{
+}
+
+
+DirichletMass::DirichletMass(
+          std::weak_ptr<const IslandMesh> mesh, 
+          const ScalarField& field,
+          std::shared_ptr<const Integrator> integrator) 
+  : Masss(std::static_pointer_cast<const AbstractMesh>(mesh.lock()), field, integrator)
+{
 }
 
--- 2d/src/fe/components/mass.hpp
+++ 2d/src/fe/components/mass.hpp
@@ -6,21 +6,62 @@
 namespace qsim2d {
 
   /*
-   * Class grouping all information needed to solve stiffness parameter
-   * of a Poisson-like equation
+   * Class grouping all information needed to solve mass parameter
+   * of elliptic equations
    */
   
-  class MassComponent : public MatrixComponent {
+  class Mass : public MatrixComponent, public FEComponent {
     
     public:
 
-      using MatrixComponent::MatrixComponent;
+      /*
+       * Initialize the mass matrix with a custom field
+       * and boundary conditions
+       */
+      Mass(
+          std::weak_ptr<const AbstractMesh> mesh, 
+          const ScalarField& field = unit_function<double>,
+          std::shared_ptr<const Integrator> integrator = std::make_shared<const Interpolator>(GAUSS_TRI_POINTS)
+          );
 
-      virtual cpx_matrix generate_matrix() const override;
-      
+      // implement matrix generation
+      virtual matrix generate_matrix() const override;
+ 
     private:
 
       // Define basis functions
       static std::array<std::function<double(const vertex_t&)>, 3> basis;
+
+      // integration
+      std::shared_ptr<const Integrator> integrator;
   };
+
+  /*
+   * Specific to neumann boundary conditions
+   */  
+  class NeumannMass : public Mass {
+    
+    public:
+      
+      // Initialize the mass matrix with neumann boundary conditions
+      NeumannMass(std::weak_ptr<const Mesh> mesh, 
+          const ScalarField& field = unit_function<double>,
+          std::shared_ptr<const Integrator> integrator = std::make_shared<const Interpolator>(GAUSS_TRI_POINTS));
+
+  };
+
+  /*
+   * Specific to dirichlet boundary conditions
+   */  
+  class DirichletMass : public Mass {
+    
+    public:
+      
+      // Initialize the mass matrix with dirichlet boundary conditions
+      DirichletMass(std::weak_ptr<const IslandMesh> mesh, 
+          const ScalarField& field = unit_function<double>,
+          std::shared_ptr<const Integrator> integrator = std::make_shared<const Interpolator>(GAUSS_TRI_POINTS));
+
+  };
+
 }
